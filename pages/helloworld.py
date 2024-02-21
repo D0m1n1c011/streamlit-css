fd3b1b155a006cbabf5e1de22ca386cf9717dd75
D0m1n1c011 <154072069+D0m1n1c011@users.noreply.github.com>
2024-02-21 11:01:05 +0800

    Update helloworld.py

--- pages/helloworld.py
+++ pages/helloworld.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from streamlit.logger import get_logger
 
+LOGGER = get_logger(__name__)
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello, world",
+        page_icon="👋",
+    )
+
+    st.write("# Hello World! 👋")
+
+run()
